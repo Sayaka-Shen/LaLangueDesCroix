5e613064fc7f3ae04a0bfb8470cb7d24f699e4ea
Gaëtan Decisy <gaetan.decisy6@hotmail.com>
2026-01-27 11:48:13 +0100

    Add content scrolling + message

---
+++ Assets/LangueDesCroix/Scripts/Gaetan/RelocateScrollView.cs
@@ -0,0 +1,21 @@
+using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelocateScrollView : MonoBehaviour
+{
+    private RectTransform m_rectTransform;
+    
+    private void Start()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(m_rectTransform);
+        UpdateScrollView();
+    }
+    
+    public void UpdateScrollView()
+    {
+        float currentHeightY = m_rectTransform.sizeDelta.y;
+        m_rectTransform.anchoredPosition = new Vector2(m_rectTransform.anchoredPosition.x, currentHeightY);
+    }
+}
