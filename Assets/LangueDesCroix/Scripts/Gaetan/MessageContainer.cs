3d43743dc593577f864a29214738b7775d60439c
Nicolas Duchesne <nicolas.duchesne@edu.devinci.fr>
2026-01-27 10:52:49 +0100

    Merge Content Scroll in UI

--- Assets/LangueDesCroix/Scripts/Gaetan/MessageContainer.cs
+++ Assets/LangueDesCroix/Scripts/Gaetan/MessageContainer.cs
@@ -5,8 +5,6 @@ public class MessageContainer : MonoBehaviour, IPointerClickHandler
 {
     [Header("Phone Manager")]
     [SerializeField] private PhoneManager m_phoneManager;
-    [SerializeField] private float m_dragMinDist = 50.0f;
-    private Vector2 m_startDragPos;
     
     public void OnPointerClick(PointerEventData pointerEventData)
     { 
