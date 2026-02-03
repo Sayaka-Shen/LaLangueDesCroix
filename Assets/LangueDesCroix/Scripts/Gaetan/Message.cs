c5acf179fa93220395351238f65d68f57a5aec1d
Nicolas Duchesne <nicolas.duchesne@edu.devinci.fr>
2026-02-03 10:27:12 +0100

    [UPDATE] Msg prefabs now take half of the width

--- Assets/LangueDesCroix/Scripts/Gaetan/Message.cs
+++ Assets/LangueDesCroix/Scripts/Gaetan/Message.cs
@@ -10,4 +10,16 @@ public class Message : MonoBehaviour
     {
         m_messageContent.text = msg;
     }
+    
+    void LateUpdate()
+    {
+        //Get rect transforms
+        RectTransform parent = transform.parent.GetComponent<RectTransform>();;
+        RectTransform rt = this.GetComponent<RectTransform>();
+        if (!parent || !rt) return;
+        
+        //We want the message to take only 70% of the width
+        float targetWidth = parent.rect.width * 0.7f;
+        rt.sizeDelta = new Vector2(targetWidth, rt.sizeDelta.y);
+    }
 }
