40e1f00960a59d6c14bf8669f62ea2345f1add08
Nicolas Duchesne <nicolas.duchesne@edu.devinci.fr>
2026-02-06 14:29:11 +0100

    [ADD] Images with traduction V1

---
+++ Assets/LangueDesCroix/Scripts/Managers/PrefabsManager.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public enum SYMBOLS
+{
+    Lost,
+    See
+}
+
+public enum IMAGES
+{
+    FirstImage,
+    SecondImage,
+    ThirdImage,
+    FourthImage,
+    FifthImage
+}
+
+public class PrefabsManager : MonoBehaviour
+{
+    public static PrefabsManager Instance { get; private set; }
+    
+    //Prefabs List
+    [SerializeField] private GameObject prefabTraduction;
+    
+    [SerializeField] private Sprite SymbolLost;
+    [SerializeField] private Sprite SymbolSee;
+    
+    [SerializeField] private Sprite FirstImage;
+    [SerializeField] private Sprite SecondImage;
+    [SerializeField] private Sprite ThirdImage;
+    [SerializeField] private Sprite FourthImage;
+    [SerializeField] private Sprite FifthImage;
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public GameObject GetPrefabTraduction()
+    {
+        return prefabTraduction;
+    }
+
+    public Sprite GetSymbol(SYMBOLS symbol)
+    {
+        switch (symbol)
+        {
+            case SYMBOLS.Lost :
+                return SymbolLost;
+            case SYMBOLS.See :
+                return SymbolSee;
+            default :
+                return null;
+        }
+    }
+    
+    public Sprite GetImage(IMAGES images)
+    {
+        switch (images)
+        {
+            case IMAGES.FirstImage :
+                return FirstImage;
+            case IMAGES.SecondImage :
+                return SecondImage;
+            case IMAGES.ThirdImage :
+                return ThirdImage;
+            case IMAGES.FourthImage :
+                return FourthImage;
+            case IMAGES.FifthImage :
+                return FifthImage;
+            default:
+                return null;
+        }
+    }
+    
+}
+
