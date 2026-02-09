8a08bd3dc191c9ba6ad7f489157c9d4f2353a6af
Letchimy Thidiane <lthidiane@gmail.com>
2026-02-09 11:44:45 +0100

    [FIX] script

--- Assets/LangueDesCroix/Scripts/Thidiane/UIManager.cs
+++ Assets/LangueDesCroix/Scripts/Thidiane/UIManager.cs
@@ -16,17 +16,17 @@ public class UIManager : MonoBehaviour
         switch (type)
         {
             case TypeBouton.Message:
-                _bgGalerie.transform.SetSiblingIndex(0);
                 _bgMessage.transform.SetSiblingIndex(2);
                 _bgNote.transform.SetSiblingIndex(1);
+                _bgGalerie.transform.SetSiblingIndex(0);
                 //_bgMessage.SetActive(true);
                 //_bgNote.SetActive(false);
                 //_bgGalerie.SetActive(false);
                 break;
             case TypeBouton.Note:
-                _bgGalerie.transform.SetSiblingIndex(0);
-                _bgMessage.transform.SetSiblingIndex(1);
                 _bgNote.transform.SetSiblingIndex(2);
+                _bgMessage.transform.SetSiblingIndex(1);
+                _bgGalerie.transform.SetSiblingIndex(0);
                 //_bgMessage.SetActive(false);
                 //_bgNote.SetActive(true);
                 //_bgGalerie.SetActive(false);
