14538e169464f170c35c732c5b3d1a7210b86056
Letchimy Thidiane <lthidiane@gmail.com>
2026-02-11 13:31:21 +0100

    [ADD] animation for UI

--- Assets/LangueDesCroix/Scripts/Thidiane/UIManager.cs
+++ Assets/LangueDesCroix/Scripts/Thidiane/UIManager.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
+using System.Collections;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject _bgMessage;
     [SerializeField] private GameObject _bgNote;
     [SerializeField] private GameObject _bgGalerie;
+
+    [Header("Message")]
+    [SerializeField] private RectTransform _rectbgMessage;
+
+    [Header("Galerie")]
+    [SerializeField] private RectTransform _rectbgGalerie;
+
+    [Header("Note")]
+    [SerializeField] private RectTransform _rectbgNote;
+
+
+    public float _durationAnim = 0.5f;
+    public float startPosXRight = 100;
+    public float startPosXLeft = -100;
+    public float EndPosX = 0;
 
     void Start()
     {
@@ -16,28 +33,33 @@ public class UIManager : MonoBehaviour
         switch (type)
         {
             case TypeBouton.Message:
+
+                _rectbgMessage.anchoredPosition = new Vector2(startPosXLeft, _rectbgMessage.anchoredPosition.y);
+
                 _bgMessage.transform.SetSiblingIndex(2);
                 _bgNote.transform.SetSiblingIndex(1);
                 _bgGalerie.transform.SetSiblingIndex(0);
-                //_bgMessage.SetActive(true);
-                //_bgNote.SetActive(false);
-                //_bgGalerie.SetActive(false);
+
+                _rectbgMessage.DOAnchorPosX(EndPosX, _durationAnim).SetEase(Ease.OutBack);
                 break;
             case TypeBouton.Note:
+                _rectbgNote.anchoredPosition = new Vector2(startPosXRight, _rectbgNote.anchoredPosition.y);
+
                 _bgNote.transform.SetSiblingIndex(2);
                 _bgMessage.transform.SetSiblingIndex(1);
                 _bgGalerie.transform.SetSiblingIndex(0);
-                //_bgMessage.SetActive(false);
-                //_bgNote.SetActive(true);
-                //_bgGalerie.SetActive(false);
+
+                _rectbgNote.DOAnchorPosX(EndPosX, _durationAnim).SetEase(Ease.OutBack);
                 break;
             case TypeBouton.Galerie:
+                _rectbgGalerie.anchoredPosition = new Vector2(startPosXRight, _rectbgGalerie.anchoredPosition.y);
+
                 _bgGalerie.transform.SetSiblingIndex(2);
                 _bgMessage.transform.SetSiblingIndex(1);
                 _bgNote.transform.SetSiblingIndex(0);
-                //_bgMessage.SetActive(false);
-                //_bgNote.SetActive(false);
-                //_bgGalerie.SetActive(true);
+
+                _rectbgGalerie.DOAnchorPosX(EndPosX, _durationAnim).SetEase(Ease.OutBack);
+
                 break;
         }
     }
