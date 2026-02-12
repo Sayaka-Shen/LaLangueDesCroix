49c56f16267bc516e6e3548cd36e1d7b49654843
Letchimy Thidiane <lthidiane@gmail.com>
2026-02-12 11:06:03 +0100

    [ADD] first sound

--- Assets/LangueDesCroix/Scripts/Thidiane/LoadingMenu.cs
+++ Assets/LangueDesCroix/Scripts/Thidiane/LoadingMenu.cs
@@ -27,7 +27,7 @@ public class LoadingMenu : MonoBehaviour
     public void Chargement()
     {
         StartCoroutine(LoadOther());
-        AudioManager.instance.PlaySFX("");
+        AudioManager.instance.PlaySFX("select_language");
     }
 
 
