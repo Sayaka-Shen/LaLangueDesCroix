859554786fedb312fac718d7718d1ad95d8dde48
Nicolas Duchesne <nicolas.duchesne@edu.devinci.fr>
2026-02-12 15:50:52 +0100

    [UPDATE] Glicthes

--- Assets/LangueDesCroix/Scripts/Translation/ImageTranslation.cs
+++ Assets/LangueDesCroix/Scripts/Translation/ImageTranslation.cs
@@ -4,4 +4,23 @@ public class ImageTranslation : MonoBehaviour
 {
     [SerializeField] public GameObject GlitchOne;
     [SerializeField] public GameObject GlitchTwo;
+    
+
+    public void  DestroyGlitchOne() 
+    {
+        Destroy(GlitchOne);
+    }
+
+    public void DestroyGlitchTwo()
+    {
+        Destroy(GlitchTwo);
+    }
+    
+    public void  FadeGlitchOne() 
+    {
+    }
+    
+    public void  FadeGlitchTwo() 
+    {
+    }
 }
