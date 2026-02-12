35246be62a569365a16ed7125b8dd73d638ca8e9
Letchimy Thidiane <lthidiane@gmail.com>
2026-02-12 11:24:11 +0100

    [EDIT] Audio

--- Assets/LangueDesCroix/Scripts/Thidiane/BoutonUIType.cs
+++ Assets/LangueDesCroix/Scripts/Thidiane/BoutonUIType.cs
@@ -9,8 +9,8 @@ public class BoutonUIType : MonoBehaviour, IPointerClickHandler
     [SerializeField] private UIManager _uiManager;
     public void OnPointerClick(PointerEventData eventData)
     {
-        _uiManager.AfficherUI(_type);
         AudioManager.instance.PlaySFX("select_language");
+        _uiManager.AfficherUI(_type);
     }
 
 
